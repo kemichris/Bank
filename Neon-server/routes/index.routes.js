1c32cd83f9c6ba329ca06dd984a952d6e0add258
kemi Chris <ekeminichristopher123@gmail.com>
2026-07-29 17:43:00 +0100

    routes

--- Neon-server/routes/index.routes.js
+++ Neon-server/routes/index.routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
 import transactionRoutes from './transaction.routes.js';
+import cardRoutes from './card.routes.js'
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/transaction', transactionRoutes);
+router.use('/card', cardRoutes);
 
 
 export default router;
