ca1b0e8fb78aac256eae7cd4baf09dd3ca80ba10
kemi Chris <ekeminichristopher123@gmail.com>
2026-08-05 21:16:17 +0100

    feat: enhance registration layout with improved styling for auth redirect section

--- Bank-client/src/components/register/RegisterLayout.jsx
+++ Bank-client/src/components/register/RegisterLayout.jsx
@@ -1,19 +1,31 @@
+import { Link } from 'react-router-dom';
+
 import { ProgressBar } from './ProgressBar';
+
 import Logo from "../../assets/images/cm-logo.png"
 
-export function RegisterLayout({ children,step }) {
+export function RegisterLayout({ children, step }) {
     return (
-            <div className='register-container'>
-                <img src={Logo} alt="" />
-                <h2>Create an Account</h2>
-                <p className='reg-p'>Join Columbia Merchant today!</p>
-                <div className='register-card'>
+        <div className='register-container'>
+            <img src={Logo} alt="" />
+            <h2>Create an Account</h2>
+            <p className='reg-p'>Join Columbia Merchant today!</p>
+            <div className='register-card'>
 
-                    <ProgressBar step={step} />
+                <ProgressBar step={step} />
 
-                    {children}
+                {children}
 
+                <div className="auth-redirect">
+                    <p>
+                        Already have an account?{' '}
+                        <Link to="/login">
+                            Login
+                        </Link>
+                    </p>
                 </div>
+
             </div>
+        </div>
     );
 }
