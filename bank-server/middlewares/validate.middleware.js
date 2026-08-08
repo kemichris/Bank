7e82cf4cb8c4e002101f3399cf337ae4c91be854
kemi Chris <ekeminichristopher123@gmail.com>
2026-08-08 16:17:02 +0100

    fix: correct import statement for ApiError utility

--- bank-server/middlewares/validate.middleware.js
+++ bank-server/middlewares/validate.middleware.js
@@ -1,5 +1,5 @@
 import { ZodError } from 'zod';
-import ApiError from '../utils/apiError.utils';
+import ApiError from '../utils/apiError.utils.js';
 
 export const validate = schema => {
     return (req, res, next) => {
