a64b67bf142f5ffcb6a97225edf5b5742d8f66aa
kemi Chris <ekeminichristopher123@gmail.com>
2026-08-11 21:10:24 +0100

    update

--- Bank-client/src/pages/user/Support.jsx
+++ Bank-client/src/pages/user/Support.jsx
@@ -2,12 +2,13 @@ import { FaArrowLeft } from "react-icons/fa";
 import { MdSupportAgent } from "react-icons/md";
 
 import { UserPageHeader } from "../../components/ui/UserPageHeader";
+import { SupportForm } from "../../components/support/SupportForm";
 
 
 export function Support() {
     return (
         <>
-            <title>Columbia Merchant | Setting </title>
+            <title>Columbia Merchant | Support </title>
             <UserPageHeader
                 cardHeader="Support Center"
                 headerDetail="Get help with your account and services"
@@ -17,6 +18,7 @@ export function Support() {
                 linkText="Back to Dashboard"
             />
 
+            <SupportForm />
             
     
         </>
