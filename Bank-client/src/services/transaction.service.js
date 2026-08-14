c3586a2b9da94e1dd37195c11ef5eb52a2db8f60
kemi Chris <ekeminichristopher123@gmail.com>
2026-08-15 00:02:17 +0100

    update

--- Bank-client/src/services/transaction.service.js
+++ Bank-client/src/services/transaction.service.js
@@ -36,13 +36,13 @@ export const internationalTransfer = async (formData) => {
 
 // Get all credit transactions 
 export const allCreditTransaction = async ()=> {
-    const {data} = await api.get('/transaction//history/credit');
+    const {data} = await api.get('/transaction/history/credit');
 
     return data 
 }
 
 export const allDebitTransaction = async ()=> {
-    const {data} = await api.get('/transaction//history/debit');
+    const {data} = await api.get('/transaction/history/debit');
 
     return data 
 }
